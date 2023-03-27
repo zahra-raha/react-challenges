e635a8f827e5f8acac31e240322279699dd7a726
Zahra Hassani <raha.zahra1375@gmail.com>
2023-03-27 06:57:31 +0000

    Add: Using Functional Components Challenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Sidebar(props)
+{
+    return <div>
+        <a href="#">My Photos</a>
+        <a href="#">My Illutrations</a>
+        <a href="#">My Paintings</a>
+    </div>
+}
+
+export default Sidebar;
