a2e85bb00055c3cbfdd1d5532224df3e8638bcfa
Zahra Hassani <raha.zahra1375@gmail.com>
2023-04-01 09:03:31 +0000

    Add Search Feature Challenge

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -2,15 +2,20 @@ import React from 'react'
 import css from "./css/Content.module.css"
 
 function PostItem(props) {
-    var post = props.post;
-  return (
-    <div className='css.SearchItem'>
-        <p>{post.title}</p>
-        <p>{post.name}</p>
-        <img src={post.image} />
-        <p>{post.description}</p>
-    </div>
-  )
+    var posts = props.savedPosts;
+    return (
+        posts.map(post =>{
+          return (
+            <div className='css.SearchItem'>
+              <p>{post.title}</p>
+              <p>{post.name}</p>
+              <img src={post.image} />
+              <p>{post.description}</p>
+          </div>
+          )
+      })
+    )
+ 
 }
 
 export default PostItem
